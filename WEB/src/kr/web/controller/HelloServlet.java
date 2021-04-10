c592f0c2d88dd26d7d5e0ad6067c32b1bce8222a
jjwon915 <jjwon915@naver.com>
2021-04-10 18:56:44 +0900

    Serlvet-mapping(web.xml) - Servlet의 경로를 노출하게 되면 실제 주소를 노출하게 되며, 보안상 좋지 않으므로 mapping해 사용한다. - web.xml에 mapping된 주소를 보고 찾아가게 된다.

---
+++ WEB/src/kr/web/controller/HelloServlet.java
@@ -0,0 +1,25 @@
+package kr.web.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import kr.web.util.MyUtil;
+
+public class HelloServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		// 1~100까지의 합 구하기
+		MyUtil myutil = new MyUtil();
+		int result = myutil.sum(100);
+		
+		// 요청한 client에게 응답하기.
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>" + result + "</h1></body>");
+		out.println("</html>");
+		out.close();
+	}
+}
