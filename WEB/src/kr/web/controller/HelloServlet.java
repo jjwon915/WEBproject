5abe1ea351b96a4af4d59467a4f630f941657c6d
jjwon915 <jjwon915@naver.com>
2021-04-13 17:23:57 +0900

    Servlet에 annotation을 사용하고 web.xml에서 삭제.

--- WEB/src/kr/web/controller/HelloServlet.java
+++ WEB/src/kr/web/controller/HelloServlet.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 import kr.web.util.MyUtil;
 
+@WebServlet("/hs.do")
 public class HelloServlet extends HttpServlet{
 	public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 		// 1~100까지의 합 구하기
